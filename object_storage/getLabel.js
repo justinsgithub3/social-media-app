86db1ecfb4b3b16aa730e4902d7b9e2eed9a0c4f
justinsgithub3 <justinsrecallback@gmail.com>
2025-10-31 18:48:14 -0400

    moved aws data to .env

--- object_storage/getLabel.js
+++ object_storage/getLabel.js
@@ -5,10 +5,10 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 // pass a random integer value as an argument into the function
 // function returns url to image
 export async function getRandomLabel(randomValue) {
-  const ACCESS_KEY = 'AKIAWOAVSWMF5LAHBRW3';
-  const SECRET_KEY = '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';
-  const BUCKET_NAME = 'test-nutrition-bucket-01';
-  const REGION = 'us-east-1';
+  const ACCESS_KEY = process.env.ACCESS_KEY;
+  const SECRET_KEY = process.env.SECRET_KEY;
+  const BUCKET_NAME = process.env.BUCKET_NAME;
+  const REGION = process.env.REGION;
 
   const s3Client = new S3Client({
       region: REGION,
@@ -37,3 +37,38 @@ export async function getRandomLabel(randomValue) {
   return url
 }
 
+
+
+// function returns a list of image urls
+export async function getDirectory() {
+  const ACCESS_KEY = process.env.ACCESS_KEY;
+  const SECRET_KEY = process.env.SECRET_KEY;
+  const BUCKET_NAME = process.env.BUCKET_NAME;
+  const REGION = process.env.REGION;
+
+  const s3Client = new S3Client({
+      region: REGION,
+      credentials: {
+          accessKeyId: ACCESS_KEY,
+          secretAccessKey: SECRET_KEY,
+      }
+  });
+
+  const main = async () => {
+    try {
+      const command = new GetObjectCommand({
+        Bucket: BUCKET_NAME,
+        Key: 'nutrition/' + randomValue.toString() + '_gator.jpg',
+      });
+      const url = await getSignedUrl(s3Client, command, {
+        expiresIn: 60, // The URL expires in 60 seconds
+      });
+      return { url };
+    } catch (err) {
+      return { err };
+    }
+  };
+
+  const url = await main();
+  return url
+}
